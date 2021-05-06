c269edeba72201d6dd2e1837d8db25a1a8a061fa
Gaurav Verma <tussharverma94@gmail.com>
2021-05-06 23:08:38 +0530

    commit on master

--- routes/clicked.js
+++ routes/clicked.js
@@ -2,9 +2,12 @@ const route = require("express").Router()
 const gpio = require("onoff").Gpio()
 
 var led = new gpio(4, 'out')
+let mssg_to_send = "LED is OFF"
 
 route.get("/", (req, res) => {
-    res.render("click.hbs")
+    res.render("click.hbs", {
+        mssg : mssg_to_send
+    })
 })
 
 route.post("/", (req, res) => {
@@ -12,14 +15,16 @@ route.post("/", (req, res) => {
     let button_val_off = req.body.led_off
     console.log("button_val_off = " + button_val_off);
     console.log("button_val_on = " + button_val_on);
-    if(button_val_off == off){
+    if(button_val_off == "off"){
         led.writeSync(0);
+        mssg_to_send = "LED is Off"
     }
-    if(button_val_on == on){
+    if(button_val_on == "on"){
         led.writeSync(1);
+        mssg_to_send = "LED is On"
     }
     res.render("click.hbs", {
-        mssg : "Completed!"
+        mssg : mssg_to_send
     })
 })
 
